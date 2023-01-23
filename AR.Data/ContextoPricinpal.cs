b8c8466cbcafbf7c27a77bf44d4693f8290cc991
Adriel Pereira <adriel.pemi@gmail>
2023-01-23 13:48:38 -0400

    Cria pasta

---
+++ AR.Data/ContextoPricinpal.cs
@@ -0,0 +1,16 @@
+﻿using AR.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AR.Data
+{
+    public class ContextoPricinpal : DbContext
+    {
+        public ContextoPricinpal(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Cliente> Cliente { get; set; }
+        
+    }
+}
